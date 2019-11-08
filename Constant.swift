dbd09b7fcb10572ddae2c9774d0461ee3cde2960
Manoj-Dev07 <45252881+Manoj-Dev07@users.noreply.github.com>
2019-11-08 18:34:01 +0530

    Create Constant.swift

---
+++ Constant.swift
@@ -0,0 +1,9 @@
+struct Constants {
+
+    static let first  = "first message"
+    static let second =  "second message"
+    static let third  =  "third message"
+    static let fourth =  "fourth message"
+    static let fifth =  "fifth message"
+
+}
